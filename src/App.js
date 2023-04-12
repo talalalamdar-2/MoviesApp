c0f1148779c8e8ad2686c4c9f51300438d617cff
talalalamdar-2 <talalalamdar@gmail.com>
2023-04-12 19:14:23 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import MovieDBLogo from './assets/movie-db-header-logo.png'
-import MoviesVideo from './assets/movies-intro.mp4'
+// import MoviesVideo from './assets/movies-intro.mp4'
 
 // import icons, and tools for react router, redux adn store
 import { connect } from "react-redux"
@@ -16,16 +16,20 @@ class App extends Component {
     return (
       <div className="App">
         <div className="header">
-          <video className="header-img" autoPlay muted loop>
-            <source src="https://movies-app-video.s3.eu-central-1.amazonaws.com/movies-intro.mp4" type="video/mp4" />
-          </video>
-
-          <div style={{ position: 'absolute', width: '100%', height: '100%', top: 0, right: 0, backgroundColor: 'rgba(59, 122, 40, 0.3)' }}>
             <img style={{ position: 'absolute', top: 8, right: 8 }} src={MovieDBLogo} alt='movie-db-logo' width={'5%'} />
 
             <header className="App-header">
               <h1 className="App-title">Movies App <MdMovie /></h1>
-            </header>
+          </header>
+
+          <div>
+
+          </div>
+
+
+          <div className="header-scroll-indicator">
+            <div>Scroll to explore</div>
+            <div className="scroll-indicator-line" />
           </div>
         </div>
 
